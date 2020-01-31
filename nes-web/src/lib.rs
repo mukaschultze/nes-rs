f3708034a5db54cee86192c3a784dad076e751de
Muka Schultze <samuelschultze@gmail.com>
2020-01-31 15:01:17 -0300

    nes-web

---
+++ nes-web/src/lib.rs
@@ -0,0 +1,96 @@
+extern crate web_sys;
+
+// A macro to provide `println!(..)`-style syntax for `console.log` logging.
+macro_rules! log {
+    ( $( $t:tt )* ) => {
+        web_sys::console::log_1(&format!( $( $t )* ).into());
+    }
+}
+
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+use nes_core::console::NesConsole;
+use nes_core::controller::Controller;
+use nes_core::controller::ControllerDataLine;
+use nes_core::palette;
+use nes_core::rom::rom_file::RomFile;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+pub struct NesWebContext {
+    nes: NesConsole,
+}
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn init() -> NesWebContext {
+    utils::set_panic_hook();
+
+    // println!("Loading ROM from {}", rom_path.display());
+    // let mut rom = RomFile::from_file(rom_path);
+    // let mut rom = RomFile::from_bytes(include_bytes!("../../roms/Super Mario Bros (E).nes"));
+    let mut rom = RomFile::from_bytes(include_bytes!("../../roms/Donkey Kong (World) (Rev A).nes"));
+    let mut nes = NesConsole::new();
+
+    nes.bus.borrow_mut().connect_cartridge(&mut rom);
+
+    {
+        let mut bus = nes.bus.borrow_mut();
+        let controller = Controller::new();
+        bus.controller0 = Some(controller);
+    }
+
+    nes.reset();
+
+    NesWebContext { nes: nes }
+}
+
+#[wasm_bindgen]
+impl NesWebContext {
+    pub fn nes_frame(&mut self) {
+        self.nes.render_full_frame();
+    }
+
+    pub fn set_image_array(&self, to_fill: &mut [u8], width: usize, height: usize) {
+        let output = &self.nes.ppu.borrow().output;
+
+        for y in 0..height {
+            for x in 0..width {
+                let color_idx = output[y * 256 + x];
+                let (r, g, b) = palette::get_rgb_color_split(color_idx);
+                let idx = y * width + x;
+                to_fill[idx * 4 + 0] = r;
+                to_fill[idx * 4 + 1] = g;
+                to_fill[idx * 4 + 2] = b;
+                to_fill[idx * 4 + 3] = 0xFF;
+            }
+        }
+    }
+
+    pub fn key_down(&mut self, key: u8) {
+        if let Some(controller) = self.nes.bus.borrow_mut().controller0.as_mut() {
+            controller
+                .data
+                .insert(ControllerDataLine::from_bits(key).unwrap());
+        }
+    }
+
+    pub fn key_up(&mut self, key: u8) {
+        if let Some(controller) = self.nes.bus.borrow_mut().controller0.as_mut() {
+            controller
+                .data
+                .remove(ControllerDataLine::from_bits(key).unwrap());
+        }
+    }
+}
---
+++ nes-web/www/index.js
@@ -0,0 +1,38 @@
+import * as nes from "nes-web";
+
+const context = nes.init();
+
+const canvas = document.getElementById("canvas");
+const canvasContext = canvas.getContext("2d");
+
+const KEYMAPS = {
+    "KeyA": 1 << 0, // A,
+    "KeyS": 1 << 1, // B,
+    "Enter": 1 << 2, // SELECT,
+    "Space": 1 << 3, // START,
+    "ArrowUp": 1 << 4, // UP,
+    "ArrowDown": 1 << 5, // DOWN,
+    "ArrowLeft": 1 << 6, // LEFT,
+    "ArrowRight": 1 << 7, // RIGHT,
+};
+
+document.addEventListener("keydown", (evt) => {
+    if (KEYMAPS[evt.code])
+        context.key_down(KEYMAPS[evt.code])
+});
+document.addEventListener("keyup", (evt) => {
+    if (KEYMAPS[evt.code])
+        context.key_up(KEYMAPS[evt.code])
+});
+
+const renderLoop = () => {
+    context.nes_frame();
+    requestAnimationFrame(renderLoop);
+
+    const imageData = canvasContext.getImageData(0, 0, canvas.width, canvas.height);
+
+    context.set_image_array(imageData.data, canvas.width, canvas.height);
+    canvasContext.putImageData(imageData, 0, 0);
+};
+
+requestAnimationFrame(renderLoop);
