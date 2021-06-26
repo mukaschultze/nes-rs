823e0abb3a1a3a0c6e9a4af4c353bfeb76846a3d
Muka Schultze <samuelschultze@gmail.com>
2021-06-26 17:49:17 -0300

    format javascript file

--- nes-web/www/index.js
+++ nes-web/www/index.js
@@ -9,50 +9,53 @@ const background = document.getElementById("background");
 const canvasContext = canvas.getContext("2d");
 
 const KEYMAPS = {
-    "KeyZ": 1 << 0, // A,
-    "KeyX": 1 << 1, // B,
-    "Enter": 1 << 2, // SELECT,
-    "Space": 1 << 3, // START,
-    "ArrowUp": 1 << 4, // UP,
-    "ArrowDown": 1 << 5, // DOWN,
-    "ArrowLeft": 1 << 6, // LEFT,
-    "ArrowRight": 1 << 7, // RIGHT,
+  KeyZ: 1 << 0, // A,
+  KeyX: 1 << 1, // B,
+  Enter: 1 << 2, // SELECT,
+  Space: 1 << 3, // START,
+  ArrowUp: 1 << 4, // UP,
+  ArrowDown: 1 << 5, // DOWN,
+  ArrowLeft: 1 << 6, // LEFT,
+  ArrowRight: 1 << 7, // RIGHT,
 };
 
 document.addEventListener("keydown", (evt) => {
-    if (KEYMAPS[evt.code])
-        context.key_down(KEYMAPS[evt.code])
+  if (KEYMAPS[evt.code]) context.key_down(KEYMAPS[evt.code]);
 });
 document.addEventListener("keyup", (evt) => {
-    if (KEYMAPS[evt.code])
-        context.key_up(KEYMAPS[evt.code])
-    if (evt.code === "KeyQ")
-        upscale = !upscale;
+  if (KEYMAPS[evt.code]) context.key_up(KEYMAPS[evt.code]);
+  if (evt.code === "KeyQ") upscale = !upscale;
 });
 
 let frames = 0;
 let fpsLog = 2000;
 
-setInterval(() => { console.log(`${frames / (fpsLog / 1000)} FPS`); frames = 0; }, fpsLog);
+setInterval(() => {
+  console.log(`${frames / (fpsLog / 1000)} FPS`);
+  frames = 0;
+}, fpsLog);
 
 const renderLoop = () => {
-    frames++;
-    requestAnimationFrame(renderLoop);
+  frames++;
+  requestAnimationFrame(renderLoop);
 
-    canvas.width = upscale ? 512 : 256;
-    canvas.height = upscale ? 480 : 240;
+  canvas.width = upscale ? 512 : 256;
+  canvas.height = upscale ? 480 : 240;
 
-    context.nes_frame();
+  context.nes_frame();
 
-    const imageData = canvasContext.getImageData(0, 0, canvas.width, canvas.height);
+  const imageData = canvasContext.getImageData(
+    0,
+    0,
+    canvas.width,
+    canvas.height
+  );
 
-    if (upscale)
-        context.set_image_array_upscale(imageData.data);
-    else
-        context.set_image_array(imageData.data);
+  if (upscale) context.set_image_array_upscale(imageData.data);
+  else context.set_image_array(imageData.data);
 
-    canvasContext.putImageData(imageData, 0, 0);
-    background.style.backgroundColor = context.get_background_color();
+  canvasContext.putImageData(imageData, 0, 0);
+  background.style.backgroundColor = context.get_background_color();
 };
 
 requestAnimationFrame(renderLoop);
